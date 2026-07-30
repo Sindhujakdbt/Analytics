42de4ea06badd8104a9066b06dda33200fc70035
sindhujakumar1606@gmail.com <sindhujakumar1606@gmail.com>
2026-07-30 07:48:00 +0000

    updated code

---
+++ models/firststage_employees.sql
@@ -0,0 +1,28 @@
+{{
+    config(materialized= 'incremental') 
+    }}
+    
+    select EMPLOYEE_ID,
+FIRST_NAME,
+LAST_NAME,
+EMAIL,
+PHONE_NUMBER,
+HIRE_DATE,
+JOB_ID,
+SALARY,
+COMMISSION_PCT,
+MANAGER_ID,
+DEPARTMENT_ID,
+current_timestamp as LOAD_TIME,
+from ods.hr.src_employees as src
+
+{% if is_incremental () %}
+where src.load_time> (select coalesce (max(load_time), '1900-01-01') from firststage_employees)
+
+{% endif %}
+
+
+
+
+
+
